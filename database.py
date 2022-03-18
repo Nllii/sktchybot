a57cb6617b0e55108f4dc786b7b27bfbf280fdd8
Nllii <ch1ris23@ymail.com>
2022-03-18 16:43:06 -0500

    updated history

---
+++ database.py
@@ -0,0 +1,24 @@
+import toml 
+import os
+from pathlib import Path
+import os
+from pathlib import Path
+import toml 
+
+credentials = toml.load(Path("credentials.toml"))
+id_= credentials['program_credentials']
+
+srv = id_['mongodb_srv']
+def save_history():
+    try:
+        from pymongo import MongoClient
+        client = MongoClient(srv)
+        db = client.sktchy
+        return db
+        # db.history.insert_one(data)
+
+    except Exception as e:
+        print(e)
+
+
+# git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch database.py" HEAD
---
+++ user.py
@@ -0,0 +1,103 @@
+import time
+import requests
+import pprint
+
+from pathlib import Path
+import toml 
+
+credentials = toml.load(Path("credentials.toml"))
+id_= credentials['program_credentials']
+
+# you will need to get the user id from the user_filtered function then call this function.
+def profile(user_id):
+    """
+    # you will need to get the user id from the user_filtered function then call this function.
+
+    """
+
+
+    cookies = {
+    }
+
+    headers = {
+        'User-Agent': 'SktchyForiPhone/3.0.0 (iPhone; iOS 14.0; Scale/3.00)',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Language': 'en-US;q=1',
+    }
+
+    data = {
+    '_appv': '465',
+    'id_': f'{user_id}',
+    'inEmail':id_['inEmail'],
+    'inPass': id_['inPass'],
+    'lang': 'en-US',
+    'zip': '1'
+    }
+
+    response = requests.post('https://api.sktchy.com/api/v1/profile/user', headers=headers, cookies=cookies, data=data)
+    # pprint.pprint(response.json())
+
+
+
+
+
+def user_filtered(usrID):
+        
+    cookies = {
+    }
+
+    headers = {
+        'User-Agent': 'SktchyForiPhone/3.0.0 (iPhone; iOS 14.0; Scale/3.00)',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Language': 'en-US;q=1',
+    }
+
+    data = {
+    '_appv': '465',
+    'inEmail':id_['inEmail'],
+    'inPass': id_['inPass'],
+    'lang': 'en-US',
+    'pp': '20',
+    't': '2',
+    'usrID': f'{usrID}',
+    'zip': '1'
+    }
+
+    response = requests.post('https://api.sktchy.com/api/v1/graph/filtered', headers=headers, cookies=cookies, data=data)
+    return response.json()
+    # pprint.pprint(response.json())
+    # for ids in  response.json()['graph/filtered']:
+    #     print(ids['_id'])
+        # return ids['_id']
+    
+    # pprint.pprint(response.json())
+
+
+def activity():
+
+    cookies = {
+    }
+
+    headers = {
+        'User-Agent': 'SktchyForiPhone/3.0.0 (iPhone; iOS 14.0; Scale/3.00)',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Language': 'en-US;q=1',
+    }
+
+    data = {
+    '_appv': '465',
+    'inEmail':id_['inEmail'],
+    'inPass': id_['inPass'],
+    'lang': 'en-US',
+    'pp': '20',
+    'zip': '1'
+    }
+
+    response = requests.post('https://api.sktchy.com/api/v1/activity/activities', headers=headers, cookies=cookies, data=data)
+    return response.json()
+
+    # pprint.pprint(response.json())
+
+
+
+# activity()
